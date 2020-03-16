6203b559bb8aea38cfc31b27795198ea28e45ec5
cdelabs <ctc.cheatz@gmail.com>
2020-03-16 14:17:29 -0500

    made it apply to pet app

--- src/main/java/com/example/demo/ConnectionController.java
+++ src/main/java/com/example/demo/ConnectionController.java
@@ -1,29 +1,105 @@
 package com.example.demo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Lazy
 @RestController
 public class ConnectionController {
-	
+
+	static int port = 8082;
+	private final CloseableHttpClient httpClient = HttpClients.createDefault();
 	Logger logger = Logger.getLogger(ConnectionController.class);
 
-	@RequestMapping("/connect")
-	public String connection() {
-		String test = new DemoHystrixCommand("hi its me").execute();
+	@RequestMapping("/**")
+	public String connection(HttpServletRequest req, HttpServletResponse resp,
+			@RequestParam(value = "lastName", required = false) String lastName) throws Exception {
+		String test = new DemoHystrixCommand(Dispatcher(req, resp, lastName)).execute();
 		return test;
 	}
-	
+
+	private String Dispatcher(HttpServletRequest req, HttpServletResponse resp, String lastName)
+			throws ConnectException {
+
+		String result = null;
+		if (req.getRequestURL().toString().contains(".png")) {
+			result = graphics(req, resp, Integer.toString(port));
+		} else {
+			result = makeConnection(req, resp, Integer.toString(port), lastName);
+		}
+		if (result == null) {
+			throw new ConnectException();
+		}
+		if (resp.getStatus() == 200)
+			System.out.println("connected to port " + (port - 1));
+		return result;
+	}
+
+	// accesses the other application
+	private String makeConnection(HttpServletRequest req, HttpServletResponse resp, String port, String lastName) {
+		System.out.println(port);
+
+		HttpGet request = new HttpGet(req.getRequestURL().toString().replaceFirst("8081", port));
+		if (lastName != null) {
+			request = new HttpGet(req.getRequestURL().toString().replaceFirst("8081", port) + "?LastName=" + lastName);
+		}
+
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+			// Get HttpResponse Status
+			System.out.println(response.getStatusLine().toString());
+			System.out.println(1);
+			HttpEntity entity = response.getEntity();
+			Header headers = entity.getContentType();
+			System.out.println(headers);
+
+			if (entity != null) {
+				String result = EntityUtils.toString(entity);
+				return result;
+			}
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String graphics(HttpServletRequest req, HttpServletResponse resp, String port) {
+		try {
+			BufferedImage image = ImageIO.read(new URL(req.getRequestURL().toString().replaceFirst("8081", port)));
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(image, "png", os);
+			InputStream is = new ByteArrayInputStream(os.toByteArray());
+
+			resp.setContentType(MediaType.IMAGE_PNG_VALUE);
+			IOUtils.copy(is, resp.getOutputStream());
+			return "done";
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+		return null;
+	}
 
 }
